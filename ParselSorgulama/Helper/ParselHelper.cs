a71d0ce765829a683b4231135f8b12ebf904ebc6
Ozanay <82051451+ozanaycelik@users.noreply.github.com>
2023-11-27 13:34:16 +0300

    Add project files.

---
+++ ParselSorgulama/Helper/ParselHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ParselSorgulama.Helper
+{
+    public static class ParselHelper
+    {
+        public static double SumValues(double y, double x)
+        {
+            return y + x;
+        }
+    }
+}
